fe72c4859f52f2bbe46c6b721bb35d67fb8f0b40
charliehay <charlie.hay@telus.com>
2019-11-29 11:36:05 -0500

    feat(12): initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import poster from "../assets/images/poster.jpg"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const containerStyle = {
+  height: `100vh`,
+  minHeight: `568px`,
+  width: `100vw`,
+  display: `flex`,
+  flexDirection: `column`,
+  alignItems: `center`,
+  justifyContent: `stretch`,
+}
+
+const imageStyle = {
+  flex: 1,
+  display: `flex`,
+  alignItems: `center`,
+  justifyContent: `center`,
+}
+
+const footerStyle = {
+  width: `100vw`,
+  background: `#D01C00`,
+  flex: `0 0 50px`,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  fontFamily: "Helvetica, Arial, sans-serif",
+  color: `#fff`,
+  padding: `0px 20px`,
+  fontSize: `.7rem`,
+}
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div style={containerStyle}>
+    <div style={imageStyle}>
+      <img src={poster} alt="12 bars" />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <footer style={footerStyle}>
+      For More Information Text Charlie At: (438) 880-5966.
+    </footer>
+  </div>
 )
 
 export default IndexPage
